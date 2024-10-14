995a41153d4af67647928759e9f40b2898d54a76
Trishan Wagle <mailtotrishan@gmail.com>
2024-10-14 21:09:39 +0545

    feat: polynomial regression in R

---
+++ Day-12/polynomial_regression.R
@@ -0,0 +1,22 @@
+dataset = read.csv("Position_Salaries.csv")
+
+linear_regressor = lm(formula = Salary ~ Level, data = dataset)
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=dataset$Level, y=dataset$Salary), color="darkgreen") +
+  geom_line(aes(x=dataset$Level, y=predict(linear_regressor, newdata = dataset)), color="red") +
+  ggtitle("Linear Regression Model for Level v/s Salary") + 
+  xlab("Level") +
+  ylab("Salary")
+y_hat_linear = predict(linear_regressor, newdata=data.frame(Level = 7.5))
+
+
+polynomial_regressor = lm(formula = Salary ~ poly(Level, 5), data = dataset)
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=dataset$Level, y=dataset$Salary), color="blue") +
+  geom_line(aes(x=dataset$Level, y=predict(polynomial_regressor, newdata = dataset)), color="red") +
+  ggtitle("Polynomial Regression Model for Level v/s Salary") + 
+  xlab("Level") +
+  ylab("Salary")
+y_hat_poly = predict(polynomial_regressor, newdata=data.frame(Level = 7.5))
